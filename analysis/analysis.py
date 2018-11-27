912dd3453ccfba5f5b3c4eca4331e0fdd123bcde
Thomas Bury <tombury182@gmail.com>
2018-11-27 12:20:51 -0500

    create python script to analyse data

---
+++ analysis/analysis.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 12:19:21 2018
+
+@author: Thomas Bury
+
+Analysis of Fussmann chemostat data using pandas
+
+"""
+
+# import python libraries
+import numpy as np
+import pandas as pd
+
+
+
+
+
+
+
+
+
+
+
