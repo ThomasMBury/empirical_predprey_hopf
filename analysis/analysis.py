af6d21992dd9494a7ac2acad1d5b00f4d0923c2c
Thomas Bury <tombury182@gmail.com>
2018-11-27 19:25:38 -0500

    shift time indices to start at 0

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -20,15 +20,35 @@ raw = pd.read_excel('../data/raw_fussmann_2000.xls',header=[1])
 # round delta column to 2d.p
 raw['meandelta'] = raw['meandelta'].apply(lambda x: round(x,2))
 
-# index by time (day number) and by meandelta (identifies experiment setting)
+# index by meandelta (identifies experiment setting)
 raw.set_index(['meandelta','day#'], inplace=True)
-                     
-# start each time-series from day 0
-raw
+
+# shift day# to start at 0
+
+# function to take list and subtract minimum element
+def zero_shift(array):
+    return array-min(array)
+
+# loop through delta values
+deltaVals = raw.index.levels[0]
+for d in deltaVals:
+    # extract data for specific delta value
+    df_temp = raw.loc[d]
+    # remove time as index
+    df_temp.reset_index(inplace=True)
+    # shift time column to start at zero
+    df_temp['day#'].apply(lambda x: x-min(df_temp['day#']))
+    # put back in dataframe
+    raw.loc[d] = df_temp
+
+
+           
+         
+
 
 
 # delta values in data
-deltaVals = raw.index.levels[0]
+
 
 
                       
