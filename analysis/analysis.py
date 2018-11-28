f8e83ad5ab782e2d1ad8124d495be569e7d3c807
Thomas Bury <tombury182@gmail.com>
2018-11-28 18:30:19 -0500

    use only delta values for series of 40 data points or more

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -45,6 +45,15 @@ for d in deltaVals:
 
 ## index dataframe by meandelta and day#
 raw.set_index(['meandelta','day#'], inplace=True)            
+
+# compute number of data points for each value of delta
+series_lengths=pd.Series(index=deltaVals)
+series_lengths.index.name= 'meandelta'
+for d in deltaVals:
+    series_lengths.loc[d] = len(raw.loc[d])
+    
+# only keep delta values with over 40 data points (so power spec can be computed)
+deltaValsFilt = series_lengths[ series_lengths>40 ].index
                
                
              
@@ -58,25 +67,35 @@ raw['Chlorella'].unstack(level=0).plot(
 raw['Brachionus'].unstack(level=0).plot(
         title = 'Brachionus trajectories')
 
-# Do EWS analysis on time-series
+# EWS analysis for Chlorella time-series
 
-# Chlorella for delta = 1.37
-series = raw.loc[1.37,'Chlorella'] 
+# Make dataframe indexed by delta with columns for each EWS
+df_ews_chlor = pd.DataFrame([])
 
-df_ews = ews_compute(series,
-                     roll_window = 1,
-                     smooth = True,
-                     band_width = 0.2,
-                     ews = ['var','ac'],
-                     lag_times = [1]
-                     )
-
+for d in deltaVals:
+    series = raw.loc[d,'Chlorella']
+    # plug series into ews_compute - no rolling window (rw=1)
+    df_ews = ews_compute(series,
+                         roll_window = 1,
+                         smooth = True,
+                         band_width = 0.2,
+                         ews = ['var','ac','smax','aic','cf','cv'],
+                         lag_times = [1,2]
+                         )
+    # Final entry of dataframe gives overall EWS for time-series (no rollwindow)
+    series_ews = df_ews.iloc[-1]
+    series_ews.name = d
+    # Add to dataframe 
+    df_ews_chlor = df_ews_chlor.append(series_ews)
+    
+    
 # Make plot of EWS for single realisation
 plt.figure(1)
 df_ews[['State variable','Smoothing']].plot(title='Early warning signals')
               
-                      
-                      
+# Take final entry of EWS dataframe (using rolling window = length of series)
+df_ews.iloc[-1]                 
+
 
 
 
