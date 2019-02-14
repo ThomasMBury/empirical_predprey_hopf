55d5f6844049ac0844bd3432acaca7deefe58867
Thomas Bury <tombury182@gmail.com>
2019-02-14 14:51:50 -0500

    bootstrap fussmann series

--- analysis/ews_bootstrap.py
+++ analysis/ews_bootstrap.py
@@ -20,7 +20,17 @@ import sys
 sys.path.append('../../early_warnings')
 from ews_compute import ews_compute
 
+# Import bootstrap module
+sys.path.append('../../ews_bootstrap')
+from roll_bootstrap import roll_bootstrap, mean_ci
 
+
+
+# Name of directory within data_export
+dir_name = 'bootstrap_block20_span05'
+
+if not os.path.exists('../data_export/'+dir_name):
+    os.makedirs('../data_export/'+dir_name)
 
 
 #-----------------------
@@ -28,12 +38,24 @@ from ews_compute import ews_compute
 #–-----------------------
 
 # EWS computation parameters
-band_width = 20 # band width of Gaussian smoothing (given as number of days)
+span = 0.5 # span used for Loess filtering of time-series
 ham_length = 40 # length of Hamming window
 ham_offset = 0.5 # offset of Hamming windows
 w_cutoff = 0.8 # cutoff of higher frequencies
-ews = ['var','ac','smax','aic','aic_params','cf','cv'] # EWS to compute
-lag_times = [1,2,10] # lag times for autocorrelation computation (lag of 10 to show decreasing AC where tau=T/2)
+ews = ['var','ac','smax','aic'] # EWS to compute
+lags = [1,2,10] # lag times for autocorrelation computation (lag of 10 to show decreasing AC where tau=T/2)
+sweep = False # whether to sweep over initialisation parameters during AIC fitting
+
+
+# Bootstrapping parameters
+block_size = 40 # size of blocks used to resample time-series
+bs_type = 'Stationary' # type of bootstrapping
+n_samples = 2 # number of bootstrapping samples to take
+roll_offset = 20 # rolling window offset (note: don't need a rolling window for Fussmann data)
+
+
+
+
 
 
 
@@ -79,293 +101,194 @@ series_lengths.index.name= 'Delta'
 for d in deltaVals:
     series_lengths.loc[d] = len(raw.loc[d])
     
-# only consider the delta values for which the corresponding trajecotories have over 25 data points
+# Only consider the delta values for which the corresponding trajecotories have over 25 data points
 deltaValsFilt = series_lengths[ series_lengths > 25 ].index
 
        
 
-#--------------------------
-# Compute EWS
-#------------------------
 
 
-# Set up a list to store output dataframes for each delta
-appended_ews = []
-appended_pspec = []
 
-# Loop through species
-for species in ['Chlorella','Brachionus']:
-    # Update status
-    print('\n'+species)
-    # Loop through delta values
-    for d in deltaValsFilt:
-        series = raw.loc[d,species]
-        # plug series into ews_compute - no rolling window (rw=1)
-        ews_dic = ews_compute(series,
-                             roll_window = 1,
-                             smooth = True,
-                             band_width = band_width,
-                             ews = ews,
-                             lag_times = lag_times,
-                             ham_length = ham_length,
-                             ham_offset = ham_offset,
-                             w_cutoff = w_cutoff
-                             )
-        # DataFrame of EWS metrics
-        df_ews_temp = ews_dic['EWS metrics']
-        # DataFrame for power spectra and their fits
-        df_pspec_temp = ews_dic['Power spectrum']
+
+
+#-------------------------------------
+# Compute bootstrapped samples of time-series data
+#–----------------------------------
+
+# Compute bootstrapped residuals of each time-series
+
+# Initialise list for DataFrames of bootstrapped time-series
+list_df_samples = []
+
+# Loop over dilution rates
+for d in deltaValsFilt:
+    # Loop over species
+    for species in ['Chlorella','Brachionus']:
         
-        # Include a column for delta value and species in each DataFrame
-        df_ews_temp['Delta'] = d
-        df_ews_temp['Species'] = species
+        # Time-series to work with
+        series = raw_traj.loc[d][species]
         
-        df_pspec_temp['Delta'] = d
-        df_pspec_temp['Species'] = species
+        # Compute bootstrapped series   
+        df_samples_temp = roll_bootstrap(series,
+                           span = span,
+                           roll_window = 1,
+                           n_samples = n_samples,
+                           bs_type = bs_type,
+                           block_size = block_size
+                           )
+
+        # Add column for dilution rate and species
+        df_samples_temp['Species'] = species
+        df_samples_temp['Dilution rate'] = d
         
-        # add DataFrames to list
-        appended_ews.append(df_ews_temp)
-        appended_pspec.append(df_pspec_temp)
+        # Remove indexing
+        df_samples_temp.reset_index(inplace=True)
         
-        # Update status    
-        print('Delta = '+str(d)+' complete.')
+        # Add DF to list
+        list_df_samples.append(df_samples_temp)
+        
+# Concatenate dataframes of samples
+df_samples = pd.concat(list_df_samples)
+
+# Drop the Time column (redundant as no rolling window used)
+df_samples.drop('Time', axis=1, inplace=True)
+
+# Set a sensible index
+df_samples.set_index(['Dilution rate', 'Species', 'Sample', 'Wintime'], inplace=True)
+
+# Sort the index
+df_samples.sort_index(inplace=True)
+
+
+        
+        
+        
+#----------------------------------------------
+# Compute EWS for each bootstrapped time-series
+#-------------------------------------------------
+
+# List to store EWS DataFrames
+list_df_ews = []
+# List to store power spectra DataFrames of one of the samples
+list_pspec = []
+
+# Sample values
+sampleVals = np.array(df_samples.index.levels[2])
+
+
+# Loop through dilution rate
+for d in deltaValsFilt:
     
-# concatenate EWS DataFrames - use species, delta and time as indices
-df_ews_full = pd.concat(appended_ews).reset_index().set_index(['Species','Delta','Time'])
-# concatenate pspec DataFrames - use species, delta, time and frequency as indices
-df_pspec = pd.concat(appended_pspec).reset_index().set_index(['Species','Delta','Time','Frequency'])
+    # Loop through species
+    for species in ['Chlorella', 'Brachionus']:
+        
+        # Loop through sample values
+        for sample in sampleVals:
+            
+            # Compute EWS of sample series
+            series_temp = df_samples.loc[(d, species, sample, ),'x']
+            
+            ews_dic = ews_compute(series_temp,
+                              roll_window = 1, 
+                              smooth = 'None',
+                              ews = ews,
+                              lag_times = lags,
+                              upto='Full',
+                              sweep=sweep,
+                              ham_length=ham_length,
+                              ham_offset=ham_offset)
+            
+            ## The DataFrame of EWS
+            df_ews_temp = ews_dic['EWS metrics']
+            
+            # Include columns for dilution rate, species and sample number
+            df_ews_temp['Dilution rate'] = d
+            df_ews_temp['Species'] = species
+            df_ews_temp['Sample'] = sample
 
-## Reduce dataframes by getting rid of NaN cells and dropping the Time index
-df_ews = df_ews_full.dropna().reset_index(level=2, drop=True) 
-df_pspec = df_pspec.reset_index(level=2, drop=True)
+            # Drop NaN values
+            df_ews_temp = df_ews_temp.dropna()        
+            
+            # Append list_df_ews
+            list_df_ews.append(df_ews_temp)
+            
+            
+            ## The DataFrame of power spectra
+            df_pspec_temp = ews_dic['Power spectrum'][['Empirical']].dropna()
+            # Add columns for species and dilution rate
+            df_pspec_temp['Dilution rate'] = d
+            df_pspec_temp['Species'] = species
+            df_pspec_temp['Sample'] = sample
+            
+            # Append list
+            list_pspec.append(df_pspec_temp)
+    
+    # Print update
+    print('EWS for d = %.2f complete' % d)
+        
+# Concatenate EWS DataFrames. Index [Dilution rate, species , Sample]
+df_ews_boot = pd.concat(list_df_ews).reset_index(drop=True).set_index(['Dilution rate','Species', 'Sample'])
+# Sort the index 
+df_ews_boot.sort_index(inplace=True)
+
+# Concatenate power spectrum DataFrames
+df_pspec_boot = pd.concat(list_pspec).reset_index().set_index(['Dilution rate','Species','Sample','Frequency'])
+# Drop the time column
+df_pspec_boot.drop('Time', inplace=True, axis=1)
+df_pspec_boot.sort_index(inplace=True)
 
 
 
 #--------------------------
-## Grid plot of all trajectories
-#--------------------------
+# Compute confidence intervals of EWS
+#-----------------------------
 
-# Set up frame and axes
-g = sns.FacetGrid(df_ews_full.reset_index(), 
-                  col='Delta',
-                  hue='Species',
-                  palette='Set1',
-                  col_wrap=3,
-                  sharey=False,
-                  aspect=1.5,
-                  height=1.8
-                  )
-# Set plot title size
-plt.rc('axes', titlesize=10)
-# Plot state variable
-g.map(plt.plot, 'Time', 'State variable', linewidth=1)
-## Plot smoothing
-#g.map(plt.plot, 'Time', 'Smoothing', color='tab:orange', linewidth=1)
-# Axes properties
-axes = g.axes
-# Assign plot label
-plot_traj = g
-## Export plot
-#plot_traj.savefig("../figures/empirical_series2.png", dpi=200)
+
+
+
+
+
+
+
+
 
 
 
 #----------------
-## Plots of EWS against delta value
+## Some plots of EWS
 #----------------
 
-# Plot of EWS metrics
-fig1, axes = plt.subplots(nrows=5, ncols=1, sharex=True, figsize=(6,6))
-df_ews.loc['Chlorella'][['Variance']].plot(ax=axes[0],title='Early warning signals')
-df_ews.loc['Brachionus'][['Variance']].plot(ax=axes[0],secondary_y=True)
-df_ews.loc['Chlorella'][['Coefficient of variation']].plot(ax=axes[1])
-df_ews.loc['Brachionus'][['Coefficient of variation']].plot(ax=axes[1],secondary_y=True)
-df_ews.loc['Chlorella'][['Lag-1 AC']].plot(ax=axes[2])
-df_ews.loc['Brachionus'][['Lag-1 AC']].plot(ax=axes[2],secondary_y=True)
-df_ews.loc['Chlorella'][['Smax']].plot(ax=axes[3])
-df_ews.loc['Brachionus'][['Smax']].plot(ax=axes[3],secondary_y=True)
-df_ews.loc['Chlorella'][['AIC hopf']].plot(ax=axes[4])
-df_ews.loc['Brachionus'][['AIC hopf']].plot(ax=axes[4],secondary_y=True)
+
+# Smax
+df_ews_boot.xs(('Chlorella'), level=('Species'))['Smax'].unstack(level='Sample').plot()
+
+# AIC hopf
+df_ews_boot.xs(('Chlorella'), level=('Species'))['AIC hopf'].unstack(level='Sample').plot()
+
+# Power spectrum
+df_pspec_boot.loc[0.04, 'Brachionus']['Empirical'].unstack(level='Sample').plot()
+
+df_pspec_boot.loc[0.04, 'Chlorella']['Empirical'].unstack(level='Sample').plot()
 
 
 
 
 
-#------------------------------
-## Plots of smoothing
-#------------------------------
-
-
-# Set up frame and axes
-g = sns.FacetGrid(df_ews_full.loc['Brachionus'].reset_index(), 
-                  col='Delta',
-                  col_wrap=3,
-                  sharey=False,
-                  aspect=1.5,
-                  height=1.8
-                  )
-# Set plot title size
-plt.rc('axes', titlesize=10)
-# Plot state variable
-g.map(plt.plot, 'Time', 'State variable', linewidth=1)
-# Plot smoothing
-g.map(plt.plot, 'Time', 'Smoothing', color='tab:orange', linewidth=1)
-# Axes properties
-axes = g.axes
-# Assign plot label
-plot_smooth = g
-
-
-
-
-#---------------------------------
-## Power spectra visualisation
-#--------------------------------
-
-# Limits for x-axis
-xmin = -2.5
-xmax = 2.5
-
-## Chlorella
-species='Chlorella'
-g = sns.FacetGrid(df_pspec.loc[species].reset_index(level=['Delta','Frequency']), 
-                  col='Delta',
-                  col_wrap=3,
-                  sharey=False,
-                  aspect=1.5,
-                  height=1.8
-                  )
-# Plots
-plt.rc('axes', titlesize=10) 
-g.map(plt.plot, 'Frequency', 'Empirical', color='k', linewidth=1)
-g.map(plt.plot, 'Frequency', 'Fit null', color='g', linestyle='dashed', linewidth=1)
-g.map(plt.plot, 'Frequency', 'Fit fold', color='b', linestyle='dashed', linewidth=1)
-g.map(plt.plot, 'Frequency', 'Fit hopf', color='r', linestyle='dashed', linewidth=1)
-
-# Axes properties
-axes = g.axes
-# Global axes properties
-for i in range(len(axes)):
-    ax=axes[i]
-    d=deltaValsFilt[i]
-    ax.set_ylim(bottom=0, top=1.1*max(df_pspec.loc[species,d]['Empirical'].loc[xmin:xmax].dropna()))
-    ax.set_xlim(left=xmin, right=xmax)
-    ax.set_xticks([-2,-1,0,1,2])
-    ax.set_title('Delta = %.2f' % deltaValsFilt[i])
-    # AIC weights
-    xpos=0.7
-    ypos=0.9
-    ax.text(xpos,ypos,
-            '$w_f$ = %.1f' % df_ews.loc[species,d]['AIC fold'],
-            fontsize=9,
-            color='b',
-            transform=ax.transAxes)  
-    ax.text(xpos,ypos-0.12,
-            '$w_h$ = %.1f' % df_ews.loc[species,d]['AIC hopf'],
-            fontsize=9,
-            color='r',
-            transform=ax.transAxes)
-    ax.text(xpos,ypos-2*0.12,
-            '$w_n$ = %.1f' % df_ews.loc[species,d]['AIC null'],
-            fontsize=9,
-            color='g',
-            transform=ax.transAxes)
-# Y labels
-for ax in axes[::3]:
-    ax.set_ylabel('Power')
+#-----------------------
+# Export data for plotting in MMA
+#-----------------------
     
-# Specific Y limits
-for ax in axes[:4]:
-    ax.set_ylim(top=0.004)
-for ax in axes[6:9]:
-    ax.set_ylim(top=0.25)
-# Assign to plot label
-pspec_plot_chlor=g
+
+# EWS data
+ews_export = ['Variance','Lag-1 AC','Lag-2 AC','Lag-10 AC','Smax','AIC fold','AIC hopf','AIC null']
+df_ews_boot[ews_export].to_csv('../data_export/'+dir_name+'/ews.csv')
+
+
+# Export empirical pspec data for plotting in MMA
+df_pspec_boot.to_csv('../data_export/'+dir_name+'/pspec.csv')
 
 
 
-
-
-## Brachionus grid plot
-species='Brachionus'
-g = sns.FacetGrid(df_pspec.loc[species].reset_index(level=['Delta','Frequency']), 
-                  col='Delta',
-                  col_wrap=3,
-                  sharey=False,
-                  aspect=1.5,
-                  height=1.8
-                  )
-# Plots
-plt.rc('axes', titlesize=10) 
-g.map(plt.plot, 'Frequency', 'Empirical', color='k', linewidth=1)
-g.map(plt.plot, 'Frequency', 'Fit null', color='g', linestyle='dashed', linewidth=1)
-g.map(plt.plot, 'Frequency', 'Fit fold', color='b', linestyle='dashed', linewidth=1)
-g.map(plt.plot, 'Frequency', 'Fit hopf', color='r', linestyle='dashed', linewidth=1)
-
-# Axes properties
-axes = g.axes
-# Global axes properties
-for i in range(len(axes)):
-    ax=axes[i]
-    d=deltaValsFilt[i]
-    ax.set_ylim(bottom=0, top=1.1*max(df_pspec.loc[species,d]['Empirical'].loc[xmin:xmax].dropna()))
-    ax.set_xlim(left=xmin, right=xmax)
-    ax.set_xticks([-2,-1,0,1,2])
-    ax.set_title('Delta = %.2f' % deltaValsFilt[i])
-    # AIC weights
-    xpos=0.7
-    ypos=0.9
-    ax.text(xpos,ypos,
-            '$w_f$ = %.1f' % df_ews.loc[species,d]['AIC fold'],
-            fontsize=9,
-            color='b',
-            transform=ax.transAxes)  
-    ax.text(xpos,ypos-0.12,
-            '$w_h$ = %.1f' % df_ews.loc[species,d]['AIC hopf'],
-            fontsize=9,
-            color='r',
-            transform=ax.transAxes)
-    ax.text(xpos,ypos-2*0.12,
-            '$w_n$ = %.1f' % df_ews.loc[species,d]['AIC null'],
-            fontsize=9,
-            color='g',
-            transform=ax.transAxes)
-# Y labels
-for ax in axes[::3]:
-    ax.set_ylabel('Power')
-    
-# Specific Y limits
-axes[1].set_ylim(top=1)
-axes[2].set_ylim(top=1)
-axes[3].set_ylim(top=3)
-axes[9].set_ylim(top=0.5)
-
-# Assign to plot label
-pspec_plot_brach=g
-
-
-
-#
-##-----------------------
-## Export data and plots
-##-----------------------
-#    
-#
-## Export pspec plots
-#pspec_plot_chlor.savefig("../figures/pspec_grid_chlor.png", dpi=200)
-#pspec_plot_brach.savefig("../figures/pspec_grid_brach.png", dpi=200)
-#
-## Export EWS data for plotting in MMA
-#cols=['Variance','Coefficient of variation','Lag-1 AC','Lag-2 AC','Lag-10 AC','Smax','AIC fold','AIC hopf','AIC null']
-#df_ews.loc['Chlorella'][cols].to_csv("../data_export/ews_chlor.csv")
-#df_ews.loc['Brachionus'][cols].to_csv("../data_export/ews_brach.csv")
-#
-## Export empirical pspec data for plotting in MMA
-#df_pspec.loc['Chlorella']['Empirical'].dropna().to_frame().to_csv('../data_export/pspec_chlor.csv',index_label=['Delta','Frequency'])
-#df_pspec.loc['Brachionus']['Empirical'].dropna().to_frame().to_csv('../data_export/pspec_brach.csv',index_label=['Delta','Frequency'])
-#
-#
-#
 
 
 
