9d4ebeeb3977fc3e267e0b044456afa24037489a
Thomas Bury <tombury182@gmail.com>
2020-02-09 23:16:25 -0500

    create roc curves for hopf bifuractions in fussmann model

---
+++ model/model_null.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16, 2019
+
+@author: ThomasMBury
+
+Code to simulate predator-prey model for Fussmann study (Science 2000)
+Null simulations (fixed parameters)
+"""
+
+#------------------------
+# Import modules
+#–----------------------
+
+# Default python modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import ewstools
+
+
+
+#----------------------
+# Useful functions
+#-----------------------
+
+# Apply operation to column of DataFrame in place
+def apply_inplace(df, field, fun):
+    return pd.concat([df.drop(field, axis=1), df[field].apply(fun)], axis=1)
+
+
+#---------------------
+# Directory for data output
+#–----------------------
+
+# Name of directory within data_export
+dir_name = 'ews_null_highhopf'
+
+if not os.path.exists('data_export/'+dir_name):
+    os.makedirs('data_export/'+dir_name)
+
+
+#--------------------------------
+# Global parameters
+#–-----------------------------
+
+# Simulation parameters
+dt = 0.01
+t0 = 0
+tmax = 400
+tburn = 100 # burn-in period
+numSims = 100
+seed = 1 # random number generation seed
+dl = 1.10 # min value of dilution rate (control)
+dh = 0.16 # max value of dilution rate (control)
+dbif = 0.151 # bifurcation value
+
+
+# EWS parameters
+dt2 = 1 # spacing between time-series for EWS computation
+rw = 0.4 # rolling window
+span = 0.2 # bandwidth
+lags = [1,3,6] # autocorrelation lag times
+ews = ['var','ac','sd','cv','skew','kurt','smax'] # EWS to compute
+ham_length = 40 # number of data points in Hamming window
+ham_offset = 0.5 # proportion of Hamming window to offset by upon each iteration
+pspec_roll_offset = 10 # offset for rolling window when doing spectrum metrics
+
+
+
+#----------------------------------
+# Model
+#----------------------------------
+
+
+# Function for model dynamics (variables [n,c,r,b])
+def de_fun(state, control, params):
+    '''
+    Inputs:
+        state: array of state variables [n,c,r,b]
+        control: control parameter that is to be varied
+        params: list of parameter values [ni,bc,kc,bb,kb,epsilon,m,lamda]
+    Output:
+        array of gradient vector (derivative)
+    '''
+    [n,c,r,b] = state
+    [ni,bc,kc,bb,kb,epsilon,m,lamda] = params
+    d = control
+    
+    # Gradients for each variable to increment by
+    n_grad = d*(ni-n) - bc*n*c/(kc+n)
+    c_grad = bc*n*c/(kc+n) - bb*c*b/((kb+c)*epsilon) - d*c
+    r_grad = bb*c*r/(kb+c) - (d+m+lamda)*r
+    b_grad = bb*c*r/(kb+c) - (d+m)*b
+            
+    return np.array([n_grad, c_grad, r_grad, b_grad])
+    
+    
+   
+# System parameters
+d=1.5 # dilution rate (control parameter)
+ni=80 # nitrogen inflow concentration
+bc=3.3 # maximum birth rate of Chlorella
+kc=4.3  # half saturation constant of Chlorella
+bb=2.25 # maximum birth rate of Brachionus
+kb=15   # half-saturation constant of Brachionus
+epsilon=0.25    # assimilation efficacy of Brachionus
+m=0.055 # mortality of Brachionus
+lamda=0.4   # decay of fecundity of Brachionus
+# Parameter list
+params = [ni,bc,kc,bb,kb,epsilon,m,lamda]
+
+
+# Noise parameters
+sigma = 0.01
+sigma_n = sigma # amplitude for N
+sigma_c = sigma # amplitude for Chlorella
+sigma_r = sigma # amplitude for R
+sigma_b = sigma # amplitude for Brachionus
+
+# Initial conditions
+n0 = 2
+c0 = 5
+r0 = 1
+b0 = 2
+# State vector
+x0 = np.array([n0,c0,r0,b0])
+
+
+#--------------------------------------------
+# Simulate (transient) realisations of model
+#-------------------------------------------
+
+
+
+# Initialise array to store time-series data
+t = np.arange(t0,tmax,dt) # Time array
+x = np.zeros([len(t), 4]) # State array
+
+# Set up control parameter d, that increases linearly in time from dl to dh
+d = pd.Series(np.linspace(dl,dh,len(t)),index=t)
+# Time at which bifurcation occurs
+#tbif = d[d > dbif].index[1]
+
+## Implement Euler Maryuyama for stocahstic simulation
+
+
+# Set seed
+np.random.seed(seed)
+
+# Initialise a list to collect trajectories
+list_traj_append = []
+
+# Loop over simulations
+print('\nBegin simulations \n')
+for j in range(numSims):
+    
+    # Create brownian increments (s.d. sqrt(dt))
+    dW_n_burn = np.random.normal(loc=0, scale=sigma_n*np.sqrt(dt), size = int(tburn/dt))
+    dW_n = np.random.normal(loc=0, scale=sigma_n*np.sqrt(dt), size = len(t)) 
+    
+    dW_c_burn = np.random.normal(loc=0, scale=sigma_c*np.sqrt(dt), size = int(tburn/dt))
+    dW_c = np.random.normal(loc=0, scale=sigma_c*np.sqrt(dt), size = len(t))
+  
+    dW_r_burn = np.random.normal(loc=0, scale=sigma_r*np.sqrt(dt), size = int(tburn/dt))
+    dW_r = np.random.normal(loc=0, scale=sigma_r*np.sqrt(dt), size = len(t))
+    
+    dW_b_burn = np.random.normal(loc=0, scale=sigma_b*np.sqrt(dt), size = int(tburn/dt))
+    dW_b = np.random.normal(loc=0, scale=sigma_b*np.sqrt(dt), size = len(t))
+    
+    # Noise vectors
+    dW_burn = np.array([dW_n_burn,
+                        dW_c_burn,
+                        dW_r_burn,
+                        dW_b_burn]).transpose()
+    
+    dW = np.array([dW_n, dW_c, dW_r, dW_b]).transpose()
+    
+    # Run burn-in period on x0
+    for i in range(int(tburn/dt)):        
+        
+        x0 = x0 + de_fun(x0, dl, params)*dt + np.dot(np.sqrt(x0),dW_burn[i])
+        # make sure that state variable remains >= 0 
+        x0 = [np.max([k,0]) for k in x0]
+        
+    # Initial condition post burn-in period
+    x[0]=x0
+    
+    # Run simulation
+    for i in range(len(t)-1):
+        x[i+1] = x[i] + de_fun(x[i], dl, params)*dt + np.dot(np.sqrt(x[i]),dW[i])
+        # make sure that state variable remains >= 0 
+        x[i+1] = [np.max([k,0]) for k in x[i+1]]
+            
+    # Store series data in a DataFrame
+    data = {'Realisation number': (j+1)*np.ones(len(t)),
+                'Time': t,
+                'Nitrogen': x[:,0],
+                'Chlorella': x[:,1],
+                'Reproducing Brachionus': x[:,2],
+                'Brachionus': x[:,3]}
+    df_temp = pd.DataFrame(data)
+    # Append to list
+    list_traj_append.append(df_temp)
+    
+    print('Simulation '+str(j+1)+' complete')
+
+#  Concatenate DataFrame from each realisation
+df_traj = pd.concat(list_traj_append)
+df_traj.set_index(['Realisation number','Time'], inplace=True)
+
+
+
+
+#----------------------
+## Execute ews_compute for each realisation and each variable
+#---------------------
+
+# Filter time-series to have time-spacing dt2
+df_traj_filt = df_traj.loc[::int(dt2/dt)]
+
+
+nC = 50000/1000000 # conversion factor to 10^6 cells/ml of Chlorella
+nB = 5 # conversion factor to females/ml of Brachiouns
+
+df_traj_filt = apply_inplace(df_traj_filt, 'Chlorella',
+                             lambda x: nC*x)
+df_traj_filt = apply_inplace(df_traj_filt, 'Reproducing Brachionus',
+                             lambda x: nB*x)
+df_traj_filt = apply_inplace(df_traj_filt, 'Brachionus',
+                             lambda x: nB*x)
+
+
+# Set up a list to store output dataframes from ews_compute
+# We will concatenate them at the end
+appended_ews = []
+appended_pspec = []
+appended_ktau = []
+
+# loop through realisation number
+print('\nBegin EWS computation\n')
+for i in range(numSims):
+    # loop through sate variable
+    for var in ['Chlorella', 'Brachionus']:
+        
+        ews_dic = ewstools.core.ews_compute(df_traj_filt.loc[i+1][var], 
+                          roll_window = rw,
+                          smooth='Lowess',
+                          span=span,
+                          lag_times = lags, 
+                          ews = ews,
+                          ham_length = ham_length,
+                          ham_offset = ham_offset,
+                          pspec_roll_offset = pspec_roll_offset,
+                          upto='Full')
+        
+        # The DataFrame of EWS
+        df_ews_temp = ews_dic['EWS metrics']
+        # The DataFrame of power spectra
+        df_pspec_temp = ews_dic['Power spectrum']
+        # The DataFrame of kendall tau values
+        df_ktau_temp = ews_dic['Kendall tau']
+        
+        
+        
+        # Include a column in the DataFrames for realisation number and variable
+        df_ews_temp['Realisation number'] = i+1
+        df_ews_temp['Variable'] = var
+        
+        df_pspec_temp['Realisation number'] = i+1
+        df_pspec_temp['Variable'] = var
+        
+        df_ktau_temp['Realisation number'] = i+1
+        df_ktau_temp['Variable'] = var
+                
+        # Add DataFrames to list
+        appended_ews.append(df_ews_temp)
+        appended_pspec.append(df_pspec_temp)
+        appended_ktau.append(df_ktau_temp)
+        
+        
+    # Print status every realisation
+    if np.remainder(i+1,1)==0:
+        print('EWS for realisation '+str(i+1)+' complete')
+
+
+# Concatenate EWS DataFrames. Index [Realisation number, Variable, Time]
+df_ews = pd.concat(appended_ews).reset_index().set_index(['Realisation number','Variable','Time'])
+# Concatenate power spectrum DataFrames. Index [Realisation number, Variable, Time, Frequency]
+df_pspec = pd.concat(appended_pspec).reset_index().set_index(['Realisation number','Variable','Time','Frequency'])
+# Concatenate kendall tau DataFrames. Index [Realisation number, Variable]
+df_ktau = pd.concat(appended_ktau).reset_index().set_index(['Realisation number','Variable'])
+
+
+
+
+
+
+##------------------------------------
+### Export data / figures
+##-----------------------------------
+#
+
+## Export the first 5 realisations to see individual behaviour
+# EWS DataFrame (includes trajectories)
+df_ews.loc[:40].to_csv('data_export/'+dir_name+'/ews_singles.csv')
+# Power spectrum DataFrame (only empirical values)
+df_pspec.loc[:40,'Empirical'].dropna().to_csv('data_export/'+dir_name+'/pspecs.csv',
+            header=True)
+df_ktau.to_csv('data_export/'+dir_name+'/ktau.csv')
+
+
---
+++ model/roc_curves/roc_fussmann_highhopf.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 11:58:49 2020
+
+Compute roc curves for EWS in the Fussmann predator prey model (without bootstrapping)
+EWS prior to low Hopf bifurcation
+Export data in a df for plotting in MMA
+
+@author: tbury
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.metrics as metrics
+
+
+
+# Function to compute ROC data from truth and indicator vals
+# and return a df.
+def roc_compute(truth_vals, indicator_vals):
+    
+    # Compute ROC curve and threhsolds using sklearn
+    fpr, tpr, thresholds = metrics.roc_curve(truth_vals,indicator_vals)
+    
+    # Compute AUC (area under curve)
+    auc = metrics.auc(fpr, tpr)
+    
+    # Put into a DF
+    dic_roc = {'fpr':fpr, 'tpr':tpr, 'thresholds':thresholds, 'auc':auc}
+    df_roc = pd.DataFrame(dic_roc)
+
+    return df_roc
+
+
+# Import Kendall tau values for Fold, Flip and Null trajectories
+stateVar = 'Chlorella'
+columns=['Variable','Variance','Lag-1 AC',
+         'Lag-3 AC','Lag-6 AC',
+         'Kurtosis','Skewness',
+         'Smax']
+df_ktau_hopf = pd.read_csv('../data_export/ews_trans_highhopf/ktau.csv',usecols=columns)[columns]
+df_ktau_null = pd.read_csv('../data_export/ews_null_highhopf/ktau.csv',usecols=columns)[columns]
+
+# Use Ktau for Chlorella
+df_ktau_hopf = df_ktau_hopf[df_ktau_hopf['Variable']==stateVar]
+df_ktau_null = df_ktau_null[df_ktau_null['Variable']==stateVar]
+
+
+# Add column for truth value
+df_ktau_hopf['Truth']=1
+df_ktau_null['Truth']=0
+
+# Combine dataframes to provide data to compute ROC
+df_roc_data = pd.concat([df_ktau_hopf,df_ktau_null],axis=0,ignore_index=True)
+
+
+
+## ROC curve data for EWS prior to Fold
+df_roc_var = roc_compute(df_roc_data['Truth'],df_roc_data['Variance'])
+df_roc_ac1 = roc_compute(df_roc_data['Truth'],df_roc_data['Lag-1 AC'])
+df_roc_ac3 = roc_compute(df_roc_data['Truth'],df_roc_data['Lag-3 AC'])
+df_roc_ac6 = roc_compute(df_roc_data['Truth'],df_roc_data['Lag-6 AC'])
+df_roc_smax = roc_compute(df_roc_data['Truth'],df_roc_data['Smax'])
+
+
+
+## Plot ROC curve for EWS prior to Hopf bifurcation
+
+plt.title('EWS prior to H1')
+plt.plot(df_roc_var['fpr'], df_roc_var['tpr'], 'b', label = 'Variance, AUC = %0.2f' % df_roc_var['auc'][0])
+plt.plot(df_roc_ac1['fpr'], df_roc_ac1['tpr'], 'g', label = 'Lag-1 AC, AUC = %0.2f' % df_roc_ac1['auc'][0])
+plt.plot(df_roc_ac3['fpr'], df_roc_ac3['tpr'], 'r', label = 'Lag-3 AC, AUC = %0.2f' % df_roc_ac3['auc'][0])
+plt.plot(df_roc_ac6['fpr'], df_roc_ac6['tpr'], 'g', label = 'Lag-6 AC, AUC = %0.2f' % df_roc_ac6['auc'][0])
+plt.plot(df_roc_smax['fpr'], df_roc_smax['tpr'], 'k', label = 'Smax, AUC = %0.2f' % df_roc_smax['auc'][0])
+#plt.plot(df_roc_sd['fpr'], df_roc_sd['tpr'], 'c', label = 'Standard deviation, AUC = %0.2f' % df_roc_sd['auc'][0])
+
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([-0.02, 1])
+plt.ylim([0, 1.02])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
+
+
+
+
+### Concatenate ROC dataframes for export
+#df_roc = pd.concat([df_roc_var, df_roc_ac1,df_roc_ac3, df_roc_ac6, df_roc_smax], axis=0)
+#df_roc.to_csv('data/roc_plotdata.csv')
+#
+#    
+
+
+
+
+
+
